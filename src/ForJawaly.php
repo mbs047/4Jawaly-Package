9da8c84e8ee9d11c776ff245a613844191528559
Development Here Ltd <38505129+devhereco@users.noreply.github.com>
2023-06-11 01:26:52 +0400

    Update ForJawaly.php

--- src/ForJawaly.php
+++ src/ForJawaly.php
@@ -6,12 +6,21 @@ use Illuminate\Support\Facades\Http;
 
 class ForJawaly
 {
-    public static function send(int $phone, ?string $message)
+    protected $base_url = "https://api-sms.4jawaly.com/api/v1";
+    protected $app_id;
+    protected $secret;
+    protected $app_hash;
+
+
+    public function __constract()
     {
-        $app_id = config('forjawaly.key');
-        $app_sec = config('forjawaly.secret');
-        $app_hash = base64_encode("{$app_id}:{$app_sec}");
-        
+        $this->app_id = config('forjawaly.key');
+        $this->secret = config('forjawaly.secret');
+        $this->app_hash = base64_encode("{$this->app_id}:{$this->secret}")
+    }
+
+    public static function send(int $phone, ?string $message)
+    {        
         $messages = [
             "messages" => [
                 [
@@ -22,11 +31,11 @@ class ForJawaly
             ]
         ];
         
-        $url = "https://api-sms.4jawaly.com/api/v1/account/area/sms/send";
+        $url = "{$this->base_url}/account/area/sms/send";
         $headers = [
             "Accept" => "application/json",
             "Content-Type" => "application/json",
-            "Authorization" => "Basic {$app_hash}"
+            "Authorization" => "Basic {$this->app_hash}"
         ];
         
         $response = Http::withHeaders($headers)->post($url, $messages);
@@ -51,14 +60,27 @@ class ForJawaly
 
     public static function balance()
     {
+        $query = [];
+        $query["is_active"] = 1;
+        $query["order_by"] = "id";
+        $query["order_by_type"] = "desc";
+        $query["page"] = 1 ;
+        $query["page_size"] = 10 ;
+        $query["return_collection"] = 1;
+
         $client = new Client();
-        $url = 'http://sms.malath.net.sa/api/getBalance.aspx?username=' . config('forjawaly.username') . '&password=' . config('forjawaly.password');
+        $response = $client->request('GET', "{$this->base_url}/account/area/me/packages", [
+            'query' => $query,
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+                "Authorization" => "Basic {$this->app_hash}"
+            ],
+        ]);
 
-        $res = $client->get($url);
-        $body = $res->getBody();
-        $smsBalance = $body->read(3);
+        $data = json_decode($response->getBody(), true);
 
-        return $smsBalance;
+        var_dump($data);
     }
 
 }
